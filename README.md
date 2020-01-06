bbd39f414d0a43d0be317c60fb236a62b7f47b0a
Emery Rose Hall <emery@deadcanaries.org>
2020-01-06 18:19:40 -0500

    i should smoke less pot

---
+++ README.md
@@ -0,0 +1,51 @@
+lorem-marxum
+============
+
+a dummy text generator for the people ☭
+
+what?
+-----
+
+what it says on the tin. like lorem ipsum, but using the communist manifesto as 
+the text source.
+
+why?
+----
+
+cus it's fuckin funny you bourgie pig.
+
+how?
+----
+
+install with npm or download a binary here somewhere. click buttons until you 
+find it.
+
+```
+npm install -g lorem-marxum
+```
+
+cool, now you can use your expensive laptop built by exploited workers to print 
+quotes from a dead white guy.
+
+```
+$ marxum --sentences 1
+The bourgeois sees in his wife a mere instrument of production.
+```
+
+wow, see, you have always needed this. try `--help` for usage. oh, also, this 
+might be useful for fuzz testing and stuff. that's actually why i made it.
+
+```
+const marxum = require('lorem-marxum');
+const words = marxum.words(6);
+const sentences = marxum.sentences(2);
+const all = marxum.all();
+
+// you get the idea
+```
+
+license
+-------
+
+licenses are for capitalists. or maybe it's gpl? can i really gpl < 50 lines of 
+code and the communist manifesto? i don't know. whatever.
---
+++ index.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env sh
+':' //; exec "$(command -v nodejs || command -v node)" "$0" "$@"
+
+/* eslint strict: 0 */ // eslint doesn't like our hack above
+
+'use strict';
+
+const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+
+class Generator {
+
+  constructor() {
+    this._source = fs.readFileSync(path.join(__dirname, 'source.txt'))
+      .toString('utf8');
+    this._parts = this._source.match(/[^\.!\?]+[\.!\?]+/g).map(s => s.trim());
+  }
+
+  words(n) {
+    const res = [];
+
+    while (res.length < n) {
+      let sentence = this.sentences(1);
+      let words = sentence.split(' ');
+
+      words.pop();
+
+      for (let word of words) {
+        if (res.length < n) {
+          res.push(word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ''));
+        }
+      }
+    }
+
+    return res.join(' ');
+  }
+
+  sentences(n) {
+    const src = this._parts.slice();
+    const res = [];
+
+    while (res.length < n) {
+      res.push(src.splice(Math.floor(Math.random() * src.length), 1));
+    }
+
+    return res.join(' ');
+  }
+
+  all() {
+    return this._source.toString();
+  }
+
+}
+
+if (require.main === module) {
+  program.version(require('./package'));
+  program.description('a dummy text generator for the people ☭');
+  program.option('-w, --words <n>', 'number words to generate');
+  program.option('-s, --sentences <n>', 'number sentences to generate');
+  program.option('-a, --all');
+  program.parse(process.argv);
+
+  const generator = new Generator();
+
+  if (program.all) {
+    process.stdout.write(generator.all());
+  } else if (program.words) {
+    process.stdout.write(generator.words(parseInt(program.words)));
+  } else if (program.sentences) {
+    process.stdout.write(generator.sentences(parseInt(program.sentences)));
+  } else {
+    program.help();
+  }
+} else {
+  modules.exports = new Generator();
+}
