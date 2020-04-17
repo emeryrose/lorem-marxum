a11b1b48db3eb00d126df5855631cf1b73e4afc6
Emery Rose Hall <emery@deadcanaries.org>
2020-04-17 19:29:09 -0400

    merge bots

---
+++ etc/api.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const app = express();
+const SlackBot = require('./slack');
+const config = JSON.parse(fs.readFileSync(process.argv[2]).toString());
+const server = https.createServer({
+  cert: fs.readFileSync(config.ServerCertificate),
+  key: fs.readFileSync(config.ServerPrivateKey)
+}, app);
+
+config.Bots.forEach(options => {
+  const bot = new SlackBot(options.SlackToken, options.SlackSigningSecret,
+    options.BotVoice);
+
+  app.use(`/${options.BotVoice}/slack/events`, bot.middleware());
+  app.get(`/${options.BotVoice}`, (req, res) => {
+    let text;
+
+    if (req.query.sentences) {
+      text = bot._lorem.sentences(parseInt(req.query.sentences));
+    } else if (req.query.words) {
+      text = bot._lorem.words(parseInt(req.query.words));
+    } else {
+      text = bot.lorem.all();
+    }
+
+    res.send(text);
+  });
+})
+
+server.listen(443);
---
+++ etc/slack.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const shuffle = require('knuth-shuffle').knuthShuffle;
+const fs = require('fs');
+const lorem = require('../index');
+const { WebClient } = require('@slack/web-api');
+const { createEventAdapter } = require('@slack/events-api');
+
+
+class SlackBot {
+
+  static get TRIGGERS() {
+    return {
+      karl: [
+        'healthcare',
+        'tuition',
+        'debt',
+        'capitalism',
+        'landlord',
+        'property',
+        'money',
+        'expensive',
+        'rent',
+        'homeless',
+        'karl',
+        'marx',
+        'mortgage',
+        'billionaire',
+        'rich',
+        'one percent',
+        'corporation',
+        'authoritarian',
+        'business',
+        'commercial',
+        'alienation',
+        'workers',
+        'real estate',
+        'investor',
+        'economy',
+        'communism',
+        'bougie',
+        'bourgeoisie',
+        'education'
+      ],
+      emma: [
+
+      ],
+      all: [
+
+      ]
+    };
+  }
+
+  constructor(slackToken, signingSecret, sourceVoice) {
+    this._web = new WebClient(slackToken);
+    this._events = createEventAdapter(signingSecret);
+    this._lorem = lorem[sourceVoice];
+    this._triggers = SlackBot.TRIGGERS[sourceVoice]
+      .concat(SlackBot.TRIGGERS.all);
+    this._lastTrigger = 0;
+
+    this._listen();
+  }
+
+  _listen() {
+    this._events.on('message', event => {
+      for (let trigger of this._triggers){
+        if (!event.text.toLowerCase().includes(trigger)) {
+          continue;
+        }
+
+        return this.reply(event.channel, trigger);
+      }
+    });
+    this._events.on('error', console.error);
+  }
+
+  async reply(channel, trigger) {
+    if (Date.now() <= this._lastTrigger + 5000) {
+      return;
+    }
+
+    let replies = [];
+
+    for (let part of this._lorem._parts) {
+      if (part.includes(trigger)) {
+        replies.push(part);
+      }
+    }
+
+    replies = shuffle(replies);
+
+    if (!replies.length) {
+      await this._web.chat.postMessage({
+        channel,
+        text: this._lorem.sentences(1)
+      });
+    } else {
+      await this._web.chat.postMessage({
+        channel,
+        text: replies.pop()
+      });
+    }
+
+    this._lastTrigger = Date.now();
+  }
+
+  middleware() {
+    return this._events.expressMiddleware();
+  }
+
+}
+
+module.exports = SlackBot;
