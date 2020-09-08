a45062856e7567e39226f86fb60fe5bab2e58a18
Emery Rose Hall <emery@deadcanaries.org>
2020-09-08 14:40:44 -0400

    play nice w heroku

--- etc/api.js
+++ etc/api.js
@@ -1,34 +1,20 @@
 'use strict';
 
+const bots = require('..');
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
+const http = require('http');
 const app = express();
-const SlackBot = require('./slack');
-const config = JSON.parse(fs.readFileSync(process.argv[2]).toString());
-const server = https.createServer({
-  cert: fs.readFileSync(config.ServerCertificate),
-  key: fs.readFileSync(config.ServerPrivateKey)
-}, app);
+// const SlackBot = require('./slack');
+const server = http.createServer(app);
 
-config.Bots.forEach(options => {
-  const bot = new SlackBot(options.SlackToken, options.SlackSigningSecret,
-    options.BotVoice);
+// const bot = new SlackBot(options.SlackToken, options.SlackSigningSecret,
+//   options.BotVoice);
 
-  app.use(`/${options.BotVoice}/slack/events`, bot.middleware());
-  app.get(`/${options.BotVoice}`, (req, res) => {
-    let text;
+// app.use(`/${options.BotVoice}/slack/events`, bot.middleware());
 
-    if (req.query.sentences) {
-      text = bot._lorem.sentences(parseInt(req.query.sentences));
-    } else if (req.query.words) {
-      text = bot._lorem.words(parseInt(req.query.words));
-    } else {
-      text = bot._lorem.all();
-    }
+app.get('/', (req, res) => res.redirect('/karl'));
+app.get(`/:voice`, (req, res) => {
+  res.send(bots[req.params.voice].sentences(1));
+});
 
-    res.send(text);
-  });
-})
-
-server.listen(443);
+server.listen(process.env.PORT || 3000);
