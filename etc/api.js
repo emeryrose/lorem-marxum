3b8f1e80b0ec84210ae063bd86f98035bef18048
Emery Rose Hall <emery@deadcanaries.org>
2020-09-08 14:59:06 -0400

    add topic query

--- etc/api.js
+++ etc/api.js
@@ -8,7 +8,17 @@ const server = http.createServer(app);
 
 app.get('/', (req, res) => res.redirect('/karl'));
 app.get(`/:voice`, (req, res) => {
-  res.send(bots[req.params.voice].sentences(1));
+  let text = bots[req.params.voice].sentences(1);
+
+  if (req.query.topic) {
+    if (bots[req.params.voice]._source.includes(req.query.topic)) {
+      while (!text.includes(req.query.topic)) {
+        text = bots[req.params.voice].sentences(1);
+      }
+    }
+  }
+
+  res.send(text);
 });
 
 server.listen(process.env.PORT || 3000);
